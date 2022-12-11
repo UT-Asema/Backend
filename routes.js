a88bccc1bd288fcda49189203abd996152293f82
sopy <doimpt@sopy.one>
2022-12-11 06:16:57 +0200

    IDFK pain

--- routes.js
+++ routes.js
@@ -9,14 +9,14 @@ const cors = require('cors')
 
 // export the routes
 module.exports = function (app) {
+  app.use(cors({credentials: true, origin: "*localhost*, *.sopy.one*", methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    preflightContinue: true, optionsSuccessStatus: 200}))
   // for each controller
   for (let controller of [loginController, postController]) {
     // for each method
     for (let method in controller) {
       // for each route
       for (let route in controller[method]) {
-        app.use(cors({credentials: true, origin: true, methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-          preflightContinue: true}))
         // get the path from the controller and route name and method
         app[method]('/' + controller.path + route, urlencodedParser,
           controller[method][route]
