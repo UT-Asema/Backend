a05195bad0b696d6dff79aa5396b65d1fcd781fa
sopy <doimpt@sopy.one>
2022-12-11 13:56:49 +0200

    header fixing

--- main.js
+++ main.js
@@ -104,5 +104,6 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin); // also  tried "*" here
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
